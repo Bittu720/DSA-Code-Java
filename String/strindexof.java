f06293d808771dbabad64f4cfa084251520f7a1f
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-03-11 09:12:48 +0530

    Add files via upload

---
+++ String/strindexof.java
@@ -0,0 +1,7 @@
+public class strindexof {
+    public static void main(String args[]){
+        String str1="bittukushwaha";
+        String str2="kushwaha";
+        System.out.println(str1.indexOf(str2));
+    }
+}
