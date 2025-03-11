ae8a18755a1f77701d56bd71aadb89a9952a6c3e
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-03-11 09:13:18 +0530

    Add files via upload

---
+++ String/strlensub.java
@@ -0,0 +1,10 @@
+class strlensub{
+    public static void main(String args[]){
+String str="bittu";
+System.out.println(str.length());
+System.out.println(str.charAt(3));
+System.out.println(str.substring(2));
+System.out.println(str.substring(1,5));
+    }  
+}
+
