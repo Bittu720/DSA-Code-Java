9afb954e7faedf93647f2dcfce55e96a42d56f52
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-03-12 10:29:18 +0530

    Add files via upload

---
+++ Array/Arrsecondlargestonepass.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Arrsecondlargestonepass {
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        int largest = -1, secondLargest = -1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+     System.out.println("enter size of array :");
+     int n=sc.nextInt();
+     int arr[]=new int[n];
+     System.out.println("enter number :");
+     for(int i=0;i<n;i++){
+         arr[i]=sc.nextInt();
+     }
+         sc.close();
+        System.out.println(getSecondLargest(arr));
+    }
+}
+ 
+
