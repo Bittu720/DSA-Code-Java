08f7af01a29bbfea397c753c5eed184869faadcc
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-04-09 22:55:53 +0530

    Add files via upload

---
+++ Array/Arr24maxsubarraysumnaive.java
@@ -0,0 +1,17 @@
+public class Arr24maxsubarraysumnaive {
+  static int MaxSubArraySum(int arr[]){
+    int res=arr[0];
+    for(int i=0;i<arr.length;i++){
+        int currSum=0;
+        for(int j=i;j<arr.length;j++){
+            currSum=currSum+arr[j];
+            res=Math.max(res,currSum);
+        }
+    }
+    return res;
+  }  
+  public static void main(String args[]){
+    int arr[]={2,3,-8,7,-1,2,3};
+    System.out.println(MaxSubArraySum(arr));
+  }
+}
