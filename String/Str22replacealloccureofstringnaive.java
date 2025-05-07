5e8c09c54a85de1dbe33d6eed5ed9214456719c8
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-05-07 16:43:11 +0530

    Add files via upload

---
+++ String/Str22replacealloccureofstringnaive.java
@@ -0,0 +1,20 @@
+public class Str22replacealloccureofstringnaive {
+ public static String replaceSubStr(String s,String s1,String s2){
+    StringBuilder ans=new StringBuilder();
+    int n=s.length(),m=s1.length();
+    for(int i=0;i<n;i++){
+        if(i+m<=n &&s.substring(i,i+m).equals(s1)){
+        ans.append(s2);
+        i+=m-1;
+        }
+        else{
+            ans.append(s.charAt(i));
+        }
+    }
+    return ans.toString();
+ } 
+ public static void main(String args[]){
+    String s="abababa",s1="aba",s2="a";
+    System.out.println(replaceSubStr(s,s1,s2));
+ }  
+}
