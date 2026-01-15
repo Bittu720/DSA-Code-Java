3d8e88382c89e985e788f582d5ee6071f2c6033f
Bittu kushwaha <bittukushwaha118@gmail.com>
2026-01-15 18:43:29 +0530

    Add files via upload

---
+++ Graph/Graph15Cheapestflightwithkstop.java
@@ -0,0 +1,14 @@
+public class Graph15Cheapestflightwithkstop {
+  static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }  
+
+}
